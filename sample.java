da065c85bf770f3a880cb312fbf41d0aa8e60b30
karthik <karthikvenkatesh@gmail.com>
2023-11-03 15:44:35 +0530

    files are uploaded

---
+++ sample.java
@@ -0,0 +1,15 @@
+class Student
+{
+int age;
+double:height;
+String:name;
+Student(int age, double height, String name)
+{
+this.age=age;
+this.height;
+this.name=name;
+}
+public static void main(String[]args) {
+Student s= new Student(22, 5.7, "karthik");
+}
+}
